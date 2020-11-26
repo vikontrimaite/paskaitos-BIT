dcbe96a529fed91c46b5697a6aa1dd5c481e89a5
Vilija Kontrimaite <vikontrimaite@gmail.com>
2020-11-26 21:49:52 +0200

    updated mini tasks with arrays, ifs and length exercises

--- 11 js intro/js/mini-tasks.js
+++ 11 js intro/js/mini-tasks.js
@@ -1,3 +1,4 @@
+// https://docs.google.com/document/d/1UnM5MzO7KPf-atlz3HXPJN8XlaN37nlUJ7fp6GwvIZ8/edit
 /* JavaScript mini užduotys */
 
 /* Kintamųjų inicijavimas
@@ -43,25 +44,29 @@ console.log(animal);
 /* Veiksmai su kintamaisiais
 
 Susumuoti visus skaičiaus tipo kintamuosius
-    Rezultatą išvesti į console
-Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
-    Rezultatą išvesti į console
-Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
-    1-2+3-4+5
-    Rezultatą išvesti į console
-Sujungti sąrašų vertes, kurių tipas yra tekstai, nuo sąrašo galo iki pradžios taip, jog tarp jų būtų kablelis ir tarpas */
+    Rezultatą išvesti į console */
 
 console.log(num1 + num2 + num3);
 
+/* Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
+    Rezultatą išvesti į console */
+
 console.log(`${num1} ${num2} ${num3}`);
 
-console.log(1 - 6 + 7 - 9 + 22);
-console.log(9 - 10 + 8 - 10 + 5);
-console.log(200 - 798 + 387 - 112 + 676);
+/* Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
+    1-2+3-4+5
+    Rezultatą išvesti į console */
 
-console.log(`${monstera}, ${banana}, ${clivia}, ${peperomia}, ${crassula}`);
-console.log(`${bed}, ${sofa}, ${chair}, ${armchair}, ${desk}`);
-console.log(`${cat}, ${bird}, ${giraffe}, ${whale}, ${dog}`);
+console.log(`Value of all ages are: ${age[0] - age[1] + age[2] - age[3] + age[4]}`);
+console.log(`Value of all marks are: ${marks[0] - marks[1] + marks[2] - marks[3] + marks[4]}`);
+console.log(`Value of all grams are: ${grams[0] - grams[1] + grams[2] - grams[3] + grams[4]}`);
+
+
+/* Sujungti sąrašų vertes, kurių tipas yra tekstai, nuo sąrašo galo iki pradžios taip, jog tarp jų būtų kablelis ir tarpas */
+
+console.log(`List of plants with space and comma: ${plants[0]}, ${plants[1]}, ${plants[2]}, ${plants[3]}, ${plants[4]}.`);
+console.log(`List of furniture with space and comma: ${furniture[0]}, ${furniture[1]}, ${furniture[2]}, ${furniture[3]}, ${furniture[4]}.`);
+console.log(`List of furniture with space and comma: ${animal[0]}, ${animal[1]}, ${animal[2]}, ${animal[3]}, ${animal[4]}.`);
 
 /* Kintamųjų palyginimas
 Lyginant, jei rezultatas tenkina palyginimo sąlygą, tai į console išvesti žodį “Pomidoras”, o jei sąlyga nėra tenkinama, išvesti sakinį “Bandykite kitą kartą.”.
@@ -72,17 +77,98 @@ Tarpusavyje palyginti skaičiaus tipo kintamuosius:
     ar jie lygūs
     ar jie nelygūs
     kuris didesnis arba lygus
-    kuris mažesnis arba lygus
-Išvesti teksto tipo kintamųjų ilgius
-Tarpusavyje palyginti teksto tipo kintamųjų ilgius:
+    kuris mažesnis arba lygus */
+
+    if (num1 > num2) {
+        console.log('Pomidoras');
+    } else {
+        console.log('Bandykite kitą kartą');
+    }
+
+    if (num2 < num3) {
+        console.log('Pomidoras');
+    } else {
+        console.log('Bandykite kitą kartą');
+    }
+
+    if (num3 === num1) {
+        console.log('Pomidoras');
+    } else {
+        console.log('Bandykite kitą kartą.');
+    }
+
+    if (num1 !== num2) {
+        console.log('Pomidoras');
+    } else {
+        console.log('Bandykite kitą kartą.');
+    }
+
+    if (num3 >= num1) {
+        console.log('Pomidoras');
+    } else {
+        console.log('Bandykite kitą kartą.');
+    }
+
+    if (num1 <= num2) {
+        console.log('Pomidoras');
+    } else {
+        console.log('Bandykite kitą kartą.');
+    }
+
+/* Išvesti teksto tipo kintamųjų ilgius */
+
+console.log(`The length of variable ${name} are ${name.length} symbols.`);
+console.log(`The length of variable ${city} are ${city.length} symbols.`);
+console.log(`The length of variable ${tea} are ${tea.length} symbols.`);
+
+/* Tarpusavyje palyginti teksto tipo kintamųjų ilgius:
     kuris didesnis
     kuris mažesnis
     ar jie lygūs
     ar jie nelygūs
     kuris didesnis arba lygus
-    kuris mažesnis arba lygus
-Išvesti sąrašo tipo kintamųjų ilgius
-Tarpusavyje palyginti sąrašo tipo kintamųjų ilgius:
+    kuris mažesnis arba lygus */
+
+    if (name.length > city.length) {
+        console.log(`Variable ${name} is longer than variable ${city}`);
+    } else {
+        console.log(`Variable ${name} is not longer than variable ${city}`);
+    }
+
+    if (city.length < tea.length) {
+        console.log(`Variable ${city} is not longer than variable ${tea}`);
+    } else {
+        console.log(`Variable ${city} is longer than variable ${tea}`);;
+    }
+
+    if (tea.length === name.length) {
+        console.log(`Variable's ${tea} length is equal to the length of variable ${name}`);
+    } else {
+        console.log(`Variable's ${tea} length is not equal to the length of variable ${name}`);
+    }
+
+    if (name.length !== city.length) {
+        console.log(`Variable's ${name} length is not equal to the length of variable ${city}`);
+    } else {
+        console.log(`Variable's ${name} length is equal to the length of variable ${city}`);
+    }
+
+    if (tea.length >= name.length) {
+        console.log(`Variable's ${tea} length is bigger or equal to the length of variable ${name}`);
+    } else {
+        console.log(`Variable's ${tea} length is not bigger or not equal to the length of variable ${name}`);
+    }
+
+    if (name.length <= city.length) {
+        console.log(`Variable's ${name} length is not bigger or not equal to the length of variable ${city}`);
+    } else {
+        console.log(`Variable's ${name} length is bigger or equal to the length of variable ${city}`);
+    }
+
+/* Išvesti sąrašo tipo kintamųjų ilgius */
+
+
+/* Tarpusavyje palyginti sąrašo tipo kintamųjų ilgius:
     kuris didesnis
     kuris mažesnis
     ar jie lygūs
@@ -108,3 +194,4 @@ Suskaičiuoti, kiek nurodytame intervale yra skaičių, kurie dalijasi be liekan
         Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 3 yra 4 vienetai.
         Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 5 yra 3 vienetai.
         Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 7 yra 2 vienetai. */
+
